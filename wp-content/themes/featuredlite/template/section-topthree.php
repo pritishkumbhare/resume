eb473b48db046e3bae8a94e31549588ccdcc05aa
Pritish Kumbhare <pritishkumbhare3@gmail.com>
2019-12-06 05:16:11 +0530

    delete all sf files

--- wp-content/themes/featuredlite/template/section-topthree.php
+++
@@ -1,7 +0,0 @@
-<?php if( shortcode_exists( 'themehunk-customizer' ) ): ?>
-<div class="container">	
-<ul class="main-feature">
-	<?php do_shortcode('[themehunk-customizer section="three-column"]'); ?>
-</ul>
-</div>
-<?php endif; ?>
--- wp-content/themes/featuredlite/template/section_testimonial.php
+++
@@ -1,9 +0,0 @@
-<?php if(!th_checkbox_filter('testimonial','section_on_off')) : 
-if( shortcode_exists( 'themehunk-customizer' ) ): ?>
-<section id="testimonials" class="client-testimonial-section">
-	<div class="container">
-		<?php do_shortcode('[themehunk-customizer section="testimonial"]'); ?>
-	</div>
-</section>
-<?php endif;
-endif; ?>
